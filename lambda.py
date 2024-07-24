d4fbd56ed2383117bab44963c26fafce4fb17cbd
Sultan Bin Mahfodh <84915642+realsulty@users.noreply.github.com>
2024-07-24 16:19:42 +0100

    Update lambda.py

--- lambda.py
+++ lambda.py
@@ -4,31 +4,33 @@ from botocore.exceptions import ClientError
 
 # Initialize the DynamoDB and SNS clients
 dynamodb = boto3.resource('dynamodb')
-table = dynamodb.Table('ServiceBooking')
+table = dynamodb.Table('FinalProjectv2')
 sns = boto3.client('sns')
 
 # Define the SNS topic ARN (replace with your actual SNS topic ARN)
-SNS_TOPIC_ARN = 'arn:aws:sns:us-west-2:637423256293:MyFinalProject'
+SNS_TOPIC_ARN = 'arn:aws:sns:us-west-2:637423256293:MyFinalProjectv2'
 
 def lambda_handler(event, context):
     try:
         body = json.loads(event['body'])
-        if 'serviceName' not in body or 'date' not in body:
+        if 'serviceName' not in body or 'date' not in body or 'email' not in body:
             raise KeyError('Missing date or service')
         
         service_name = body['serviceName']
         date = body['date']
+        email = body['email']
         
         # Insert item into DynamoDB
         response = table.put_item(
             Item={
                 'serviceName': service_name,
-                'date': date
+                'date': date,
+                'email': email
             }
         )
         
         # Prepare the message for SNS
-        message = f"A new Appointment has been booked: {date} for service: {service_name}"
+        message = f" User {email} has a new Appointment that been booked at : {date} for service: {service_name}"
         
         # Publish the message to SNS
         sns_response = sns.publish(
